2a786e4767051e11ff229bd784e4b82b369badd7
trochepablo <trochepablodaniel@gmail.com>
2019-11-29 18:33:26 -0300

    appNode fix

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+ 
+const express = require('express');
+const exec = require('child_process').execSync;
+ 
+// Constants
+const PORT = 8080;
+ 
+// App
+const app = express();
+app.get('/', function (req, res) {
+  var result=""+exec( "ping -c 1 google.com" );
+ 
+  while ( result.indexOf("\n")!=-1 )
+    result=result.replace("\n","<br/>");
+ 
+  res.send( '<h1>Test ping</h1>'+result );
+});
+ 
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
